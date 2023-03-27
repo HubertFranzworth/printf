d89aa5f03c1ebf218c4185a61a0ec26043ab6b7c
HubertFranzworth <122721807+HubertFranzworth@users.noreply.github.com>
2023-03-27 13:42:38 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -93,3 +96,6 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
+
+
+#endif
